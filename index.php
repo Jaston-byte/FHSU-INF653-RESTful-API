b836266c72910582dba50a2bab7ade1c1b6787de
Jaston Alba <jaston.alba@gmail.com>
2023-03-19 13:22:58 -0600

    Edited index file in root, added text to display

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
-<?php
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    $method = $_SERVER['REQUEST_METHOD'];
-
-    if ($method === 'OPTIONS') {
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-        exit();
-    }
+<h1>Jaston Alba</h1>
+<h3>Midterm Project RESTful API</h3>
