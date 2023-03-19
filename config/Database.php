97ca0befaabdf25602994275d067809a4bc5edf6
Jaston Alba <jaston.alba@gmail.com>
2023-03-19 14:35:08 -0600

    Added environmental vars to DB config

--- config/Database.php
+++ config/Database.php
@@ -2,18 +2,22 @@
 class Database
 {
     // DB params
-    private $host = '';
-    private $port = '';
-    private $db_name = '';
-    private $username = '';
-    private $password = '';
+    private $host;
+    private $port;
+    private $db_name;
+    private $username;
+    private $password;
     private $conn;
 
     // DB connect
     public function connect()
     {
+        $this->host = getenv("HOST");
+        $this->port = getenv("PORT");
+        $this->db_name = getenv("DB_NAME");
+        $this->username = getenv("USERNAME");
+        $this->password = getenv("PASSWORD");
         $this->conn = null;
-
         $dsn = 'pgsql:host=' . $this->host . ';port=' . $this->port .
             ';dbname=' . $this->db_name;
         try {
